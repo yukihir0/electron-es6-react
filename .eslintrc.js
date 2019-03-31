b8ae2badd854311ab36180498f9db3b8100eb9e7
yukihir0 <yukihiro.cotori@gmail.com>
2019-03-31 17:07:00 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+  }
+};
--- src/main/index.js
+++ src/main/index.js
@@ -1,25 +1,25 @@
-const {app, BrowserWindow} = require("electron")
-let window
+const {app, BrowserWindow} = require("electron");
+let window;
 
 function createWindow () {
-  window = new BrowserWindow({width: 800, height: 600})
-  window.loadFile("src/index.html")
-  window.webContents.openDevTools()
+  window = new BrowserWindow({width: 800, height: 600});
+  window.loadFile("src/index.html");
+  window.webContents.openDevTools();
   window.on("closed", () => {
-    win = null
+    window = null;
   })
 }
 
-app.on("ready", createWindow)
+app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on("activate", () => {
   if (window === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
